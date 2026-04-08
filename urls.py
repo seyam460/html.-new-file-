a9e503de597326fa6e6e47dc96abf45f1a61b5ce
seyam460 <sseyam450@gmail.com>
2026-04-09 02:34:04 +0600

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,24 +1,11 @@
-"""
-URL configuration for config project.
+from django .urls import path 
+from .views import task_list, add_task, edit_task, toggle_task, delete_task
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path,include 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path ("tasks/", include("tasks.urls")),
-    
+    path ("task_list/", task_list, name = "task_list"),
+    path("add_task/", add_task, name="add_task"),
+    path("edit_task/<int:id>/", edit_task, name="edit_task"),
+    path("toggle_task/<int:id>/", toggle_task, name="toggle_task"),
+    path("delete_task/<int:id>/", delete_task, name="delete_task"),
 ]
